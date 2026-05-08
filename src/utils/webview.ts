d9bfaa81faff17da85ed9367b29dbfce11e6f1b4
Francisco Lou <franciscogardenberg@gmail.com>
2026-05-08 17:16:12 -0300

    chore: add icon

--- src/utils/webview.ts
+++ src/utils/webview.ts
@@ -14,7 +14,7 @@ export function openWebview(
     );
     panel.iconPath = vscode.Uri.joinPath(
         context.extensionUri,
-        ...'assets/icon/pytree.svg'.split("/")
+        ...'assets/images/pytree.svg'.split("/")
     );
     panel.webview.html = html;
 
