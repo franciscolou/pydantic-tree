c2ac04b05ed7de532f3ea07e5f872f6848cb53fe
Francisco Lou <franciscogardenberg@gmail.com>
2026-05-08 11:55:37 -0300

    fix: remove duplicate Tree word in scanning files message

--- src/config/messages.ts
+++ src/config/messages.ts
@@ -10,7 +10,7 @@ export const Messages = {
     },
 
     status: {
-        scanningFiles: 'PyTree Tree: scanning files...',
+        scanningFiles: 'PyTree: scanning files...',
     },
 
     webView: {
